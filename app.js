88efacdc223ba2e551383518774acdbe8e4d984f
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2020-09-21 18:43:10 -0500

    Initial commit.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -20,4 +22,18 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+// app.get('/set-cookies', (req,  res) => {
+//   // res.setHeader('Set-Cookie', 'newUser=true');
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, secure: true});
+//   res.send('You got your cookie(s).');
+// });
+
+// app.get('/read-cookies', (req, res) => {
+//   const cookies = req.cookies;
+//   console.log(cookies);
+//   res.json(cookies);
+// });
+
 app.use(authRoutes);
